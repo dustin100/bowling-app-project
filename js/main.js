92319ef2c991e80c2ccdd20518b23fd4464ce9b7
Dustin <dmkelly100@gmail.com>
2019-08-16 20:54:16 -0400

    removed refactor-jquery.js and put all js into main.js

--- js/main.js
+++ js/main.js
@@ -14,10 +14,10 @@ $(function () {
         "nine",
         "ten"
     ]),
-    (bowlingApp.keepScore = 0);
-    bowlingApp.time = 99;
+    bowlingApp.score = 0;
+    bowlingApp.time = 60;
     bowlingApp.answer = [];
-
+    bowlingApp.isUserPlaying = false;
     bowlingApp.randomNumber = function () {
         num = Math.floor(Math.random() * 2);
         if (num === 0) {
@@ -27,26 +27,80 @@ $(function () {
         }
     };
 
-    // this is how to start the game. After clikcing start button it will run all functions to start game
     bowlingApp.init = function () {
-        $("#start-button").on('click', function () {
-            //functions will go here
-            bowlingApp.answer = [];
-            bowlingApp.getPinNumber();
-            bowlingApp.getShowPin();
-            bowlingApp.getAnswer();
-            bowlingApp.getPersonAnswer();
-        });
+        bowlingApp.score = 0;
+        bowlingApp.time = 60;
+        bowlingApp.answer = [];
+        bowlingApp.isUserPlaying = false;
+    }
+
+    $("#start-button").on('click', function () {
+        bowlingApp.startGame();
+    });
+
+
+    // this is how to start the game. After clikcing start button it will run all functions to start game
+    bowlingApp.startGame = function () {
+        bowlingApp.answer = [];
+        bowlingApp.isUserPlaying = true;
+        bowlingApp.getPinNumber();
+        bowlingApp.getShowPin();
+        bowlingApp.getAnswer();
+        bowlingApp.startCount();
+
+        bowlingApp.checkPlaying();
+        console.log(bowlingApp.answer);
     };
 
-    // creates new array called pinNumber that now holds a boolean for each pin
+    bowlingApp.checkPlaying = function () {
+        if (bowlingApp.isUserPlaying === true) {
+            $('#start-button button').prop(
+                'disabled', true).text('Good Luck!').css({
+                color: 'green',
+                weight: 'bold',
+                background: '#d2d2ca',
+                width: '150px',
+                cursor: 'auto',
+                border: 'none'
+
+            });
+        } else {
+            $('#start-button button').prop('disabled', false)
+        }
+    }
+
+    // function that starts and stops  the countdown timer
+    bowlingApp.startCount = function () {
+        bowlingApp.interval = setInterval(countDown, 1000);
+
+        function countDown() {
+            bowlingApp.time--
+            $('#count-down-time').text(bowlingApp.time)
+            if (bowlingApp.time <= 0) {
+                clearInterval(bowlingApp.interval);
+                bowlingApp.stopGame();
+                bowlingApp.finalScore();
+            }
+        }
+    }
+
+    // this allows you to keep playing after you get the answer right
+    bowlingApp.keepPlaying = function () {
+        bowlingApp.answer = [];
+        bowlingApp.getPinNumber();
+        bowlingApp.getShowPin();
+        bowlingApp.getAnswer();
+        console.log(bowlingApp.answer)
+    }
+
+    // creates new array called pinNumber that  holds a boolean for each pin
     bowlingApp.getPinNumber = function () {
         this.pinNumber = this.pinNumbers.map(function () {
             return bowlingApp.randomNumber();
         });
     };
 
-    //
+
     bowlingApp.getShowPin = function () {
         this.pinNumber.forEach(function (item, index) {
             if (item === true) {
@@ -77,29 +131,86 @@ $(function () {
 
         return true;
     };
-    
+
 
     bowlingApp.plusScore = function () {
-        document.getElementById("score").innerHTML = this.keepScore += 12;
+        bowlingApp.score += 12;
+        $('#score').text(bowlingApp.score);
     };
+
+
     bowlingApp.minusScore = function () {
-        document.getElementById("score").innerHTML = this.keepScore -= 3;
-    };
-
-    bowlingApp.getPersonAnswer = function () {
-        console.log(this.answer);
-        this.personAnswer = prompt(`Enter the Pins Left Standing`).split(",");
-
-        while (this.checkEqual(this.answer, this.personAnswer) === false) {
-            this.minusScore();
-            this.personAnswer = prompt(`Enter the Pins Left Standing`).split(",");
+        if (bowlingApp.score <= 0) {
+            bowlingApp.score = 0
+        } else {
+            bowlingApp.score -= 3;
         }
-        this.plusScore();
-        console.log(`You're Right`);
+        $('#score').text(bowlingApp.score);
     };
+
+
+    // stop form from refreshing the page
+    bowlingApp.getPersonAnswer = $('form').on('submit', function (e) {
+        e.preventDefault()
+        // grab form inut from user 
+        bowlingApp.personAnswer = $('#person-answer').val().split(',');
+        console.log(bowlingApp.personAnswer);
+
+
+        // checks if user's answer matches the correct answer
+        if (bowlingApp.checkEqual(bowlingApp.answer, bowlingApp.personAnswer) === false) {
+            bowlingApp.minusScore();
+            $('.display-correctness').text('Try Again').css({
+                display: 'block',
+                color: 'red'
+            }).fadeToggle(700);
+        } else {
+            bowlingApp.plusScore();
+            $('.display-correctness').text('Right!').css({
+                display: 'block',
+                color: '#7dc243'
+            }).fadeToggle(700);
+            bowlingApp.keepPlaying();
+        };
+        //clears previous entry
+        $('#person-answer').val('');
+    });
+
+    // Will display your final score at the end of the game
+    bowlingApp.finalScore = function () {
+        $('.display-correctness').text(`Final Score ${bowlingApp.score}`).css({
+            display: 'block',
+            color: '#7dc243'
+        })
+    };
+
+
+
+    //stops the game by disabling the input fields 
+    bowlingApp.stopGame = function () {
+        $('.disable-elements').attr(
+            'disabled', true).css('cursor', 'auto');
+        $('#play-again-button button').css('display', 'block');
+    }
+
+    $('#play-again-button').on('click', function () {
+        $('#play-again-button button').hide();
+        $('.display-correctness').hide();
+        $('.disable-elements').attr('disabled', false);
+        $('#score').text('0');
+
+        bowlingApp.init();
+        bowlingApp.isUserPlaying = true;
+        bowlingApp.getPinNumber();
+        bowlingApp.getShowPin();
+        bowlingApp.getAnswer();
+        bowlingApp.startCount();
+        bowlingApp.resetTimer();
+        bowlingApp.checkPlaying();
+        console.log(bowlingApp.answer);
+
+    })
 
     bowlingApp.init();
 
-    // check out https://stackoverflow.com/questions/7410063/how-can-i-listen-to-the-form-submit-event-in-javascript/7410112#7410112
-
-})
+});
