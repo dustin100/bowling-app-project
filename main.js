3d187c056a56a53571f3bdf8b70f3447d658659b
Dustin <dmkelly100@gmail.com>
2019-08-16 20:24:54 -0400

    refactor js for name spacing

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-const pinNumbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"];
+const bowlingApp = {};
+bowlingApp.pinNumbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"],
+  bowlingApp.keepScore = 0;
+bowlingApp.time = 99;
+bowlingApp.answer = [];
 
-
-
-const randomNumber = () => {
+bowlingApp.randomNumber = function () {
   num =
     Math.floor(Math.random() * 2);
   if (num === 0) {
@@ -10,65 +12,79 @@ const randomNumber = () => {
   } else {
     return false;
   }
-
 }
-let score = 10;
-const scoring = () => {
-  document.getElementById("score").innerHTML = score;
-}
-scoring();
 
-//resets pins shown
-const startButton = document.getElementById("start-button");
-startButton.onclick = () => {
+// this is how to start the game. After clikcing start button it will run all functions to start game
+bowlingApp.init = function () {
+  const startButton = document.getElementById("start-button");
+  startButton.onclick = function () {
+    //functions will go here 
+    bowlingApp.getPinNumber();
+    bowlingApp.getShowPin();
+    bowlingApp.getAnswer();
+    bowlingApp.getPersonAnswer();
+  }
+};
 
-  const pinNumber = pinNumbers.map(function () {
-    return randomNumber();
+// creates new array called pinNumber that now holds a boolean for each pin  
+bowlingApp.getPinNumber = function () {
+  this.pinNumber = this.pinNumbers.map(function () {
+    return bowlingApp.randomNumber();
   });
+}
 
-  const showPin = pinNumber.forEach(function (item, index) {
+//
+bowlingApp.getShowPin = function () {
+  this.pinNumber.forEach(function (item, index) {
     if (item === true) {
-      return document.getElementById(pinNumbers[index]).style.visibility = "visible";
+      return document.getElementById(bowlingApp.pinNumbers[index]).style.visibility = "visible";
     } else {
-      return document.getElementById(pinNumbers[index]).style.visibility = "hidden";
+      return document.getElementById(bowlingApp.pinNumbers[index]).style.visibility = "hidden";
     }
   });
-  //create answer by pushing index into new array 
-  let answer = [];
-
-  const checkAnswer = pinNumber.map(function (item, index) {
-    if (item === true)
-      answer.push(pinNumbers[index]);
-
-  });
-
-  //function that checks if two arrays are equal
-
-  const checkEqual = (arr1, arr2) => {
-    if (arr1.length !== arr2.length)
-      return false;
-    for (var i = arr1.length; i--;) {
-      if (arr1[i] != arr2[i])
-        return false;
-    }
-
-    return true;
-
-  }
-
-
-
-
-  console.log(answer);
-  let personAnswer = prompt(`Enter the Pins Left Standing`).split(",");
-
-  while (checkEqual(answer, personAnswer) === false) {
-    score--
-    scoring();
-    personAnswer = prompt(`Enter the Pins Left Standing`).split(",");
-  }
-  console.log(`You're Right`)
-  score = score + 5
-  scoring();
-  console.log(personAnswer);
 }
+
+//pushs the correct answer into new array
+bowlingApp.getAnswer = function () {
+  this.pinNumber.map(function (item, index) {
+    if (item === true)
+      bowlingApp.answer.push(bowlingApp.pinNumbers[index]);
+
+  });
+}
+
+//function that checks if two arrays are equal
+bowlingApp.checkEqual = function (arr1, arr2) {
+  if (arr1.length !== arr2.length)
+    return false;
+  for (var i = arr1.length; i--;) {
+    if (arr1[i] != arr2[i])
+      return false;
+  }
+
+  return true;
+
+}
+
+
+bowlingApp.plusScore = function () {
+  document.getElementById("score").innerHTML = (this.keepScore += 10) * 1.3;
+}
+bowlingApp.minusScore = function () {
+  document.getElementById("score").innerHTML = this.keepScore -= 3;
+}
+
+bowlingApp.getPersonAnswer = function () {
+  console.log(this.answer);
+  this.personAnswer = prompt(`Enter the Pins Left Standing`).split(",");
+
+  while (this.checkEqual(this.answer, this.personAnswer) === false) {
+
+    this.minusScore();
+    this.personAnswer = prompt(`Enter the Pins Left Standing`).split(",");
+  }
+  this.plusScore();
+  console.log(`You're Right`)
+}
+
+bowlingApp.init();
